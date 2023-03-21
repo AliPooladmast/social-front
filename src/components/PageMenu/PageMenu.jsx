cf98be52b0a790c30a361787284fd5c606dd566a
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-03-22 01:45:08 +0430

    add page menu as the navbar core component

---
+++ src/components/PageMenu/PageMenu.jsx
@@ -0,0 +1,89 @@
+import {
+  HowToReg,
+  Login,
+  Logout,
+  PlaylistAddCheck,
+  Work,
+} from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import { logout } from "../../redux/userSlice";
+import { userRequest } from "../../requestMethods";
+
+const PageMenu = ({
+  ItemClassName,
+  TitlesClassName,
+  SelectedClassName,
+  currentUser,
+}) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    userRequest.defaults.headers.token = "";
+  };
+
+  return (
+    <>
+      <div
+        className={`${ItemClassName} ${
+          location.pathname === "/jobs" ? SelectedClassName : ""
+        }`}
+        onClick={() => navigate("/jobs")}
+      >
+        <span className={TitlesClassName}>Jobs</span>
+        <Work />
+      </div>
+
+      {currentUser ? (
+        <>
+          <div
+            className={`${ItemClassName} ${
+              location.pathname === "/applications" ? SelectedClassName : ""
+            }`}
+            onClick={() => navigate("/applications")}
+          >
+            <span className={TitlesClassName}>Applications</span>
+            <PlaylistAddCheck />
+          </div>
+
+          <div
+            className={`${ItemClassName} ${
+              location.pathname === "/login" ? SelectedClassName : ""
+            }`}
+            onClick={handleLogout}
+          >
+            <span className={TitlesClassName}>Logout</span>
+            <Logout />
+          </div>
+        </>
+      ) : (
+        <>
+          <div
+            className={`${ItemClassName} ${
+              location.pathname === "/register" ? SelectedClassName : ""
+            }`}
+            onClick={() => navigate("/register")}
+          >
+            <span className={TitlesClassName}>Register</span>
+            <HowToReg />
+          </div>
+
+          <div
+            className={`${ItemClassName} ${
+              location.pathname === "/login" ? SelectedClassName : ""
+            }`}
+            onClick={() => navigate("/login")}
+          >
+            <span className={TitlesClassName}>Sign In</span>
+            <Login />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default PageMenu;
