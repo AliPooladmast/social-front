ac990fa28a90850e8c83798212e95baa1c629ea0
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-03-21 16:39:53 +0330

    add user api calls

---
+++ src/redux/apiCalls.js
@@ -0,0 +1,72 @@
+import { publicRequest, userRequest } from "../requestMethods";
+import { loginSuccess, userFailure, userStart } from "./userSlice";
+import { setMessage } from "./uxSlice";
+
+//User API Calls
+export const login = async (dispatch, user) => {
+  dispatch(userStart());
+  try {
+    const res = await publicRequest.post("/auth/login", user);
+    if (res) {
+      userRequest.defaults.headers.token = "Bearer " + res.data?.token;
+      dispatch(loginSuccess(res.data));
+      dispatch(
+        setMessage({
+          type: "success",
+          text: "welcome! " + res.data?.username?.toString(),
+        })
+      );
+    }
+  } catch (err) {
+    dispatch(userFailure());
+    dispatch(
+      setMessage({ type: "error", text: err?.response?.data?.toString() })
+    );
+  }
+};
+
+export const register = async (dispatch, user) => {
+  dispatch(userStart());
+  try {
+    const res = await publicRequest.post("/auth/register", user);
+    if (res) {
+      dispatch(loginSuccess(res.data));
+      dispatch(
+        setMessage({
+          type: "success",
+          text:
+            res.data?.username?.toString() +
+            " account has been created successfully",
+        })
+      );
+    }
+  } catch (err) {
+    dispatch(userFailure());
+    dispatch(
+      setMessage({ type: "error", text: err?.response?.data?.toString() })
+    );
+  }
+};
+
+export const editUser = async (dispatch, userId, user) => {
+  dispatch(userStart());
+  try {
+    const res = await userRequest.put("/users/" + userId, user);
+    if (res) {
+      dispatch(loginSuccess(res.data));
+      dispatch(
+        setMessage({
+          type: "success",
+          text:
+            res.data?.username?.toString() +
+            " account has been edited successfully",
+        })
+      );
+    }
+  } catch (err) {
+    dispatch(userFailure());
+    dispatch(
+      setMessage({ type: "error", text: err?.response?.data?.toString() })
+    );
+  }
+};
