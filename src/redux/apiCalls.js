d2c4c9c78fbd0a7c5d9944a86b85f2bce4a53c49
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-03-22 15:51:38 +0430

    add application api calls

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,4 +1,10 @@
 import { publicRequest, userRequest } from "../requestMethods";
+import {
+  applicationStart,
+  addApplicationSuccess,
+  applicationFailure,
+  getApplicationSuccess,
+} from "./applicationSlice";
 import { loginSuccess, userFailure, userStart } from "./userSlice";
 import { setMessage } from "./uxSlice";
 
@@ -65,6 +71,42 @@ export const editUser = async (dispatch, userId, user) => {
     }
   } catch (err) {
     dispatch(userFailure());
+    dispatch(
+      setMessage({ type: "error", text: err?.response?.data?.toString() })
+    );
+  }
+};
+
+//Application API Calls
+export const getApplications = async (dispatch, userId) => {
+  dispatch(applicationStart());
+  try {
+    const res = await userRequest.get("applications/find/" + userId);
+    dispatch(getApplicationSuccess(res?.data));
+  } catch (err) {
+    dispatch(applicationFailure());
+    dispatch(
+      setMessage({ type: "error", text: err?.response?.data?.toString() })
+    );
+  }
+};
+
+export const addApplication = async (dispatch, data) => {
+  dispatch(applicationStart());
+  try {
+    const res = await userRequest.post("/applications", data);
+
+    if (res) {
+      dispatch(addApplicationSuccess(res.data));
+      dispatch(
+        setMessage({
+          type: "success",
+          text: "Your application has been sent successfully",
+        })
+      );
+    }
+  } catch (err) {
+    dispatch(applicationFailure());
     dispatch(
       setMessage({ type: "error", text: err?.response?.data?.toString() })
     );
